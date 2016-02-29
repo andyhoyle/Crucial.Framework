ca71032b78a8be2f0c298535ed2a40e3f6b281c8
unknown <a@Inspiron.hoyl.es>
2016-02-29 19:00:00 +0000

    Initial Orleankka implementation of User, Organisation and Facility

---
+++ DesignPatterns/Orleankka/CqsActor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crucial.Framework.DesignPatterns.Orleankka
+{
+    public abstract class CqsActor : Actor
+    {
+        public override Task<object> OnReceive(object message)
+        {
+            var cmd = message as Command;
+            if (cmd != null)
+                return HandleCommand(cmd);
+
+            var query = message as Query;
+            if (query != null)
+                return HandleQuery(query);
+
+            throw new InvalidOperationException("Unknown message type: " + message.GetType());
+        }
+
+        protected abstract Task<object> HandleCommand(Command cmd);
+        protected abstract Task<object> HandleQuery(Query query);
+    }
+}
