0b5a208a7277073228b299079b99322f7bb3bca9
Andy Hoyle <a@hoyl.es>
2016-05-11 16:36:49 +0100

    Booking functionality, slot conversions and linq extensions

---
+++ Extensions/Linq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crucial.Framework.Extensions
+{
+    public static class Linq
+    {
+        public static bool ConsecutiveValidator<T, TResult>(
+            this IEnumerable<T> instance,
+            Func<T, TResult> selector,
+            Func<TResult, TResult, bool> isAdjacentFunc)
+        {
+            return instance.Select((item, index) => { return (index == (instance.Count() - 1)) || isAdjacentFunc(selector.Invoke(item), selector.Invoke(instance.ElementAt(index + 1))); }).All(b => b);
+        }
+    }
+}
